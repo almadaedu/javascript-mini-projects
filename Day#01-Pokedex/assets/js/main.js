faefbc2ae8e3e490e71f9a6c29cee278251ce0f6
Eduardo Almada <56881337+almadaedu@users.noreply.github.com>
2023-05-17 08:03:57 -0300

    feat: Load more button

--- Day#01-Pokedex/assets/js/main.js
+++ Day#01-Pokedex/assets/js/main.js
@@ -1,23 +1,52 @@
-const pokemonList = document.getElementById("pokemonList");
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
 
+const maxRecords = 151
+const limit = 10
+let offset = 0;
 
 function convertPokemon(pokemon) {
   return `
-    <li class="pokemon ${pokemon.type}">
-        <span class="number">#00${pokemon.number}</span>
-        <span class="name">${pokemon.name}</span>
-
-        <div class="detail">
-            <ol class="types">
-                ${pokemon.types.map((type) => `<li class="type  ${type}">${type}</li>`).join(' ')}
-            </ol>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.photo}.png"
-                alt="${pokemon.name}">
-        </div>
-    </li>
-    `;
+  <li class="pokemon ${pokemon.type}">
+      <span class="number">#00${pokemon.number}</span>
+      <span class="name">${pokemon.name}</span>
+  
+      <div class="detail">
+          <ol class="types">
+              ${pokemon.types.map((type) => `<li class="type  ${type}">${type}</li>`).join(' ')}
+          </ol>
+          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.photo}.png"
+              alt="${pokemon.name}">
+      </div>
+  </li>
+  `;
 }
 
-pokeApi.getPokemons().then((pokemons = []) => {
-  pokemonList.innerHTML += pokemons.map(convertPokemon).join('')
-});
+function loadPokemonCards(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemon).join('')
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+loadPokemonCards(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordsWithNexPage = offset + limit
+
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonCards(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+      loadPokemonCards(offset, limit)
+    }
+})
+
+
+
+
+
+
